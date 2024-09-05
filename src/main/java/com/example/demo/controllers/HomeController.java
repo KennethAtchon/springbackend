957b90f3ee62c3359204e044e83a75fdf252fb36
KenenthAtchon <kenneth.atchon@hotmail.com>
2024-09-05 15:07:24 -0500

    changes

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -2,15 +2,13 @@ package com.example.demo.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("message", "Hello, Spring ABoot with Thymeleaf!");
+    public String home() {
         return "home";
     }
 }
